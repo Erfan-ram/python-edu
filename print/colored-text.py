e5010644b236b43a990093002fe81cb7430ce7a6
Erfan-ram <Erfanramezany245@gmail.com>
2023-03-13 17:42:56 +0330

    completed colored text

--- print/colored-text.py
+++ print/colored-text.py
@@ -1,7 +1,13 @@
-from Font import RESET, Color, Background
+from Font import RESET, Color, Background, Style
 
 print(Background['Yellow'] + Color['Red'] + "this is a Red text" + RESET)
 
 
-for color, code in Color.items():
-    print(f"{code} this is {color} text  {RESET}")
+for __color, code in Color.items():
+    print(f"{code} this is {__color} text  {RESET}")
+
+for __background, code in Background.items():
+    print(f"{code} this is {__background} background  {RESET}")
+
+for __style, code in Style.items():
+    print(f"{code} this is {__style} text  {RESET}")
