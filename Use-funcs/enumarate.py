14a45ea667f29e2d476f05ee68ced8f3e1ca43f1
Erfan-ram <Erfanramezany245@gmail.com>
2023-05-10 18:07:12 +0330

    completed enumarate

--- Use-funcs/enumarate.py
+++ Use-funcs/enumarate.py
@@ -2,3 +2,7 @@ sentence = "hey it is something called a sentence"
 
 for index, item in enumerate(sentence):
     print(f"word {item} indexed at {index}")
+
+split_sen= sentence.split()
+
+print(f"\nsentence has {len(split_sen)} words in it")
