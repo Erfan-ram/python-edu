b82f12f825e79873305c340d06259698c26a2216
Erfan-ram <Erfanramezany245@gmail.com>
2023-05-15 19:43:24 +0330

    some code for args

--- Function/args-kwargs.py
+++ Function/args-kwargs.py
@@ -3,4 +3,14 @@ def avg(*args):
 
 
 test = avg(10, 10, 12)
-print(f" avg of is/are{test[0]}")
+print(f" avg of {test[1]} is/are {test[0]}")
+
+
+def example(**kwarg):
+    if 'numbers' in kwarg:
+        [sum(kwarg['numbers'])/len(kwarg['numbers']),[values for values in kwarg['numbers']]]
+
+ask=input("please enter some numbers to get avg").split()
+test2 = avg([int(any) for any in ask])
+
+print(f" avg of {test2[1]} is/are {test2[0]}")
