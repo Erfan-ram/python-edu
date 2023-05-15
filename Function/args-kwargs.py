503d2fd32a4b67067767ed3dd8f39298d6fd8c17
Erfan-ram <Erfanramezany245@gmail.com>
2023-05-15 19:51:02 +0330

    finalized kwargs parameters syntax

--- Function/args-kwargs.py
+++ Function/args-kwargs.py
@@ -6,11 +6,12 @@ test = avg(10, 10, 12)
 print(f" avg of {test[1]} is/are {test[0]}")
 
 
-def example(**kwarg):
+def avg2(**kwarg):
     if 'numbers' in kwarg:
-        [sum(kwarg['numbers'])/len(kwarg['numbers']),[values for values in kwarg['numbers']]]
+        return [sum(kwarg['numbers'])/len(kwarg['numbers']), [values for values in kwarg['numbers']]]
 
-ask=input("please enter some numbers to get avg").split()
-test2 = avg([int(any) for any in ask])
+
+ask = input("\nplease enter some numbers to get avg :  ").split()
+test2 = avg2(numbers=[int(any) for any in ask])
 
 print(f" avg of {test2[1]} is/are {test2[0]}")
