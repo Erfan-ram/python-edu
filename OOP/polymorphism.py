f7f5f344ba25b91dd956c5a2886a3fc231f34b1f
Erfan-ram <Erfanramezany245@gmail.com>
2023-05-18 01:00:38 +0330

    poly finalized

--- OOP/polymorphism.py
+++ OOP/polymorphism.py
@@ -1,18 +1,21 @@
 class Animal:
     def __init__(self, name):
         self.name = name
-    
+
     def make_sound(self):
-        pass
-        
+        raise ChildProcessError("it should be implemented in child class")
+
+
 class Dog(Animal):
     def make_sound(self):
         return "Woof"
-        
+
+
 class Cat(Animal):
     def make_sound(self):
         return "Meow"
-        
+
+
 animals = [Dog("Rufus"), Cat("Whiskers"), Dog("Buddy"), Cat("Socks")]
 
 for animal in animals:
